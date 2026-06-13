36877f612436afbb8325b4122566401cbebffcd4
Khushubansal29 <khushubansal2006@gmail.com>
2026-06-14 02:27:12 +0530

    Move model training pipeline into standalone script

---
+++ src/train.py
@@ -0,0 +1,33 @@
+import pandas as pd
+import joblib
+
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+
+data = pd.read_csv(
+    "../dataset/for-2sec/for-2seconds/features/audio_features.csv"
+)
+
+X = data.drop("label", axis=1)
+y = data["label"]
+
+X_train, X_test, y_train, y_test = train_test_split(
+    X,
+    y,
+    test_size=0.2,
+    random_state=42
+)
+
+model = RandomForestClassifier(
+    n_estimators=100,
+    random_state=42
+)
+
+model.fit(X_train, y_train)
+
+joblib.dump(
+    model,
+    "../model/random_forest_model.pkl"
+)
+
+print("Model trained and saved")
