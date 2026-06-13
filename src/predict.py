cd4bcf9fbc33a453c1e073f16eee4255c518705e
Khushubansal29 <khushubansal2006@gmail.com>
2026-06-14 02:05:51 +0530

    Convert model predictions to readable fake and real labels

--- src/predict.py
+++ src/predict.py
@@ -6,6 +6,9 @@ model = joblib.load("../model/random_forest_model.pkl")
 def predict_audio(file_path):
     features = extract_features(file_path)
 
-    prediction = model.predict([features])
+    prediction = model.predict([features])[0]
 
-    return prediction[0]
+    if prediction == 0:
+        return "fake"
+    else:
+        return "real"
